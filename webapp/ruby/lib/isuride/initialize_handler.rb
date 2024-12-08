fd1263d2739bdf4eeedbe553524af5b995c6f68f
koyo-miyamura <nexus9071@gmail.com>
2024-12-08 14:54:40 +0900

    total_distances テーブル

--- webapp/ruby/lib/isuride/initialize_handler.rb
+++ webapp/ruby/lib/isuride/initialize_handler.rb
@@ -18,6 +18,29 @@ module Isuride
 
       db.xquery("UPDATE settings SET value = ? WHERE name = 'payment_gateway_url'", req.payment_server)
 
+      db.xquery('DROP TABLE IF EXISTS chair_total_distances')
+
+      db.xquery(<<~SQL)
+        CREATE TABLE chair_total_distances AS
+        SELECT
+            chair_id,
+            SUM(distance) AS total_distance,
+            MAX(created_at) AS total_distance_updated_at
+        FROM (
+            SELECT
+                chair_id,
+                created_at,
+                IFNULL(ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
+                ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)), 0) AS distance
+            FROM
+                chair_locations
+        ) AS subquery
+        GROUP BY
+            chair_id
+      SQL
+
+      db.xquery('ALTER TABLE chair_total_distances ADD UNIQUE INDEX `idx_chair_id` (`chair_id`)')
+
       json(language: 'ruby')
     end
   end
